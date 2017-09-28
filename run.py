914019923759395cd46f45a624d44a9ef5930ce6
tsurumeso <nstm101339@gmail.com>
2017-09-29 02:43:19 +0900

    Add some options

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 
 import chainer
 import chainer.functions as F
@@ -8,23 +8,39 @@ import cv2
 import numpy as np
 
 
-model = VGG16Layers()
-img = cv2.imread(sys.argv[1], 1)
-acts = model.extract((img[:, :, ::-1],), layers=['conv5_3', 'prob'])
+def main():
+    model = VGG16Layers()
+    img = cv2.imread(args.input, 1)
+    acts = model.extract((img[:, :, ::-1],), layers=[args.layer, 'prob'])
 
-one_hot = np.zeros((1, 1000), dtype=np.float32)
-one_hot[:, 282] = 1
-one_hot = F.sum(chainer.Variable(one_hot) * acts['prob'])
-one_hot.backward(retain_grad=True)
+    one_hot = np.zeros((1, 1000), dtype=np.float32)
+    if args.label == -1:
+        one_hot[:, acts['prob'].data.argmax()] = 1
+    else:
+        one_hot[:, args.label] = 1
+    loss = F.sum(chainer.Variable(one_hot) * acts['prob'])
+    loss.backward(retain_grad=True)
 
-weights = np.mean(acts['conv5_3'].grad.data, axis=(2, 3))
-cam = np.zeros(acts['conv5_3'].data.shape[2:])
-for w, fmap in zip(weights[0], acts['conv5_3'].data[0]):
-    cam += w * fmap
-cam /= cam.max()
-cam = (cam > 0) * cam
-cam = cv2.resize(np.uint8(cam * 255), (224, 224))
+    weights = np.mean(acts[args.layer].grad.data, axis=(2, 3))
+    cam = np.zeros(acts[args.layer].data.shape[2:])
+    for w, fmap in zip(weights[0], acts[args.layer].data[0]):
+        cam += w * fmap
+    cam /= cam.max()
+    cam = (cam > 0) * cam
+    cam = cv2.resize(np.uint8(cam * 255), (224, 224))
 
-heatmap = cv2.applyColorMap(cam, cv2.COLORMAP_JET)
-img = img * 0.5 + heatmap + 0.5
-cv2.imwrite('heatmap.png', img)
+    heatmap = cv2.applyColorMap(cam, cv2.COLORMAP_JET)
+    img = img * 0.5 + heatmap + 0.5
+    cv2.imwrite('heatmap.png', img)
+
+
+p = argparse.ArgumentParser()
+p.add_argument('--input', '-i', default='images/boxer_cat.png')
+p.add_argument('--gpu', '-g', type=int, default=-1)
+p.add_argument('--label', '-l', type=int, default=-1)
+p.add_argument('--layer', default='conv5_3')
+args = p.parse_args()
+
+
+if __name__ == '__main__':
+    main()
